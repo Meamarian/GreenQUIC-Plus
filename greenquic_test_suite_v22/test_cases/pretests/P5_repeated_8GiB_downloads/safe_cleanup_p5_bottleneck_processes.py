32e2a4969997ca68b54dae37ac323741c8d5bd9d
Meamarian <60857359+Meamarian@users.noreply.github.com>
2026-08-17 19:11:42 +0200

    P5 bottleneck: clean diagnostic and V2 sweep processes safely

--- greenquic_test_suite_v22/test_cases/pretests/P5_repeated_8GiB_downloads/safe_cleanup_p5_bottleneck_processes.py
+++ greenquic_test_suite_v22/test_cases/pretests/P5_repeated_8GiB_downloads/safe_cleanup_p5_bottleneck_processes.py
@@ -5,7 +5,9 @@ import safe_cleanup_greenquic_processes as base
 
 base.CMD_SUBSTRINGS = tuple(base.CMD_SUBSTRINGS) + (
     '/P5_repeated_8GiB_downloads/run_p5_parallel_off_case.sh',
+    '/P5_repeated_8GiB_downloads/run_p5_bottleneck_case_diag.sh',
     '/P5_repeated_8GiB_downloads/run_p5_bottleneck_sweep.sh',
+    '/P5_repeated_8GiB_downloads/run_p5_bottleneck_sweep_v2.sh',
     '/tmp/P5_BOTTLENECK_SWEEP_',
     '/tmp/P5_BOTTLENECK_',
     'cpu_busy_sampler.py',
